204a0797407d378c8226eee86dfb75d7bfe0f07d
4ancer <yurii.yesyp@gmail.com>
2021-05-13 12:22:27 +0300

    test mods

--- src/Layout/Wrappers/appLayout.vue
+++ src/Layout/Wrappers/appLayout.vue
@@ -16,7 +16,6 @@
       </transition>
     </div>
     <!-- Modal Component -->
-    <!-- Modal Component -->
     <b-modal id="modal1" title="Bootstrap-Vue">
       <p class="my-4">Hello from modal!</p>
     </b-modal>
