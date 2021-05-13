0a3cfeee245af7d777305e471e9ee2359601df61
4ancer <yurii.yesyp@gmail.com>
2021-05-13 12:14:50 +0300

    test

--- src/Layout/Wrappers/appLayout.vue
+++ src/Layout/Wrappers/appLayout.vue
@@ -16,6 +16,7 @@
       </transition>
     </div>
     <!-- Modal Component -->
+    <!-- Modal Component -->
     <b-modal id="modal1" title="Bootstrap-Vue">
       <p class="my-4">Hello from modal!</p>
     </b-modal>
--- src/Layout/Wrappers/baseLayout.vue
+++ src/Layout/Wrappers/baseLayout.vue
@@ -17,6 +17,7 @@
         </transition>
       </div>
       <!-- Modal Component -->
+      <!-- sdd -->
       <b-modal id="modal1" title="Bootstrap-Vue">
         <p class="my-4">Hello from modal!</p>
       </b-modal>
